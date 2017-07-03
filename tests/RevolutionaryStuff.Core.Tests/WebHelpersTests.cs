cbfb461975590ab5de44133c0adb70c242c6929b
Jason <jason@jasonthomas.com>
2017-07-03 18:09:34 -0400

    Fixing a nuget versioning issue

---
+++ tests/RevolutionaryStuff.Core.Tests/WebHelpersTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RevolutionaryStuff.Core.Tests
+{
+    [TestClass]
+    public class WebHelpersTests
+    {
+        [TestClass]
+        public class CreateHttpContentMethodTests
+        {
+            [TestMethod]
+            public void WhenGivenStringCreateHttpContent()
+            {
+                var testString = "NOT NULL";
+                Assert.IsNotNull(testString);
+
+                //object nullObject = null;
+                //Assert.IsNotNull(nullObject);
+            }
+        }
+    }
+}
