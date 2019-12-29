b3b3d2b2ed8667823264c24bc8c242c9ff5b179f
Jason Thomas <jason@jasonthomas.com>
2019-12-29 14:55:10 -0500

    AspNetCore helper library

---
+++ src/RevolutionaryStuff.AspNetCore/AspHelpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RevolutionaryStuff.AspNetCore
+{
+    public static class AspHelpers
+    {
+        public static string GetDisplayName(this Enum value)
+            => value.GetDisplayAttribute()?.GetName() ?? value.ToString();
+
+        public static string GetDescription(this Enum value)
+            => value.GetDisplayAttribute()?.GetDescription();
+
+        private static DisplayAttribute GetDisplayAttribute(this Enum value)
+        {
+            var type = value.GetType();
+            if (!type.IsEnum) throw new ArgumentException(String.Format("Type '{0}' is not Enum", type));
+
+            var members = type.GetMember(value.ToString());
+            if (members.Length == 0) throw new ArgumentException(String.Format("Member '{0}' not found in type '{1}'", value, type.Name));
+
+            var member = members[0];
+            var attributes = member.GetCustomAttributes(typeof(DisplayAttribute), false);
+            return (DisplayAttribute)attributes.FirstOrDefault();
+        }
+
+        public static IList<SelectListItem> ConvertToSelectList<TStruct>(this IEnumerable<TStruct> itemsToConvert) where TStruct : struct
+        {
+            var stringsSelect = new List<SelectListItem>();
+
+            foreach (var stringToConvert in itemsToConvert)
+            {
+                var val = stringToConvert.ToString();
+                stringsSelect.Add(new SelectListItem { Text = val, Value = val });
+            }
+            return stringsSelect;
+        }
+
+        /// <remarks>https://rburnham.wordpress.com/2015/03/13/asp-net-mvc-defining-scripts-in-partial-views/</remarks>
+        public static HtmlString Script(this IHtmlHelper htmlHelper, Func<object, Microsoft.AspNetCore.Mvc.Razor.HelperResult> template)
+        {
+            htmlHelper.ViewContext.HttpContext.Items["_script_" + Guid.NewGuid()] = template;
+            return HtmlString.Empty;
+        }
+
+        /// <remarks>https://rburnham.wordpress.com/2015/03/13/asp-net-mvc-defining-scripts-in-partial-views/</remarks>
+        public static HtmlString RenderPartialViewScripts(this IHtmlHelper htmlHelper)
+        {
+            foreach (object key in htmlHelper.ViewContext.HttpContext.Items.Keys)
+            {
+                if (key.ToString().StartsWith("_script_"))
+                {
+                    var template = htmlHelper.ViewContext.HttpContext.Items[key] as Func<object, Microsoft.AspNetCore.Mvc.Razor.HelperResult>;
+                    if (template != null)
+                    {
+                        htmlHelper.ViewContext.Writer.Write(template(null));
+                    }
+                }
+            }
+            return HtmlString.Empty;
+        }
+    }
+}
---
+++ src/RevolutionaryStuff.AspNetCore/Filters/HttpHeadersFilter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using RevolutionaryStuff.Core;
+
+namespace RevolutionaryStuff.AspNetCore.Filters
+{
+    public class HttpHeadersFilter : IAsyncActionFilter
+    {
+        public class Config
+        {
+            public const string ConfigSectionName = "HttpHeadersFilterConfig";
+
+            public bool IncludeMachineName { get; set; }
+            public bool IncludeServerTime { get; set; }
+            public bool IncludeEnvironmentInformation { get; set; }
+            public TimeSpan IdleLogout { get; set; }
+            public IDictionary<string, string> AdditionalHeaders { get; set; }
+        }
+
+        private readonly IOptions<Config> ConfigOptions;
+        private readonly IWebHostEnvironment Host;
+
+        public HttpHeadersFilter(IOptions<Config> configOptions, IWebHostEnvironment host)
+        {
+            Requires.NonNull(configOptions, nameof(configOptions));
+            ConfigOptions = configOptions;
+            Host = host;
+        }
+
+        async Task IAsyncActionFilter.OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var c = ConfigOptions.Value;
+            var headers = context.HttpContext.Response.Headers;
+            if (!headers.ContainsKey(WebHelpers.HeaderStrings.CacheControl))
+            {
+                headers[WebHelpers.HeaderStrings.CacheControl] = "no-cache, no-store";
+            }
+            if (c.IncludeMachineName)
+            {
+                headers["x-MachineName"] = Environment.MachineName;
+            }
+            if (c.IncludeServerTime)
+            {
+                headers["x-ServerTime"] = DateTime.Now.ToString();
+                headers["x-ServerTimeUtc"] = DateTime.UtcNow.ToString();
+            }
+            if (c.IncludeEnvironmentInformation)
+            {
+                headers["x-Environment"] = Host.EnvironmentName;
+                headers["x-ApplicationName"] = Host.ApplicationName;
+            }
+            if (c.AdditionalHeaders != null)
+            {
+                foreach (var kvp in c.AdditionalHeaders)
+                {
+                    headers[kvp.Key] = kvp.Value;
+                }
+            }
+            context.HttpContext.Response.Cookies.Append("loggedIn", context.HttpContext.User.Identity.IsAuthenticated ? "true" : "false");
+            context.HttpContext.Response.Cookies.Append("sessionTimeoutInSeconds", c.IdleLogout.TotalSeconds.ToString());
+            await next();
+        }
+    }
+}
