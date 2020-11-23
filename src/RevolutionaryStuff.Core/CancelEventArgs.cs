600d9797d4a7cb4f288e0eca8cdb1584b25b7f77
Jason Thomas <jason@jasonthomas.com>
2020-11-23 11:06:32 -0400

    Last pre 5 nuget

---
+++ src/RevolutionaryStuff.Core/CancelEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace RevolutionaryStuff.Core
+{
+    public class CancelEventArgs<T> : CancelEventArgs
+    {
+        public readonly T Data;
+
+        [DebuggerStepThrough]
+        public CancelEventArgs(T data) => this.Data = data;
+    }
+}
