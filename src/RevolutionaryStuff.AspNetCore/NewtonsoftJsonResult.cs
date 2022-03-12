9546d15874b8f09c31e511a7781c5cc2e09e57e6
Jason Thomas <jason@traffk.com>
2022-03-12 00:25:25 -0500

    fix a few using issues

--- src/RevolutionaryStuff.AspNetCore/NewtonsoftJsonResult.cs
+++ src/RevolutionaryStuff.AspNetCore/NewtonsoftJsonResult.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json;
 
 namespace Microsoft.AspNetCore.Mvc;
 
-
 public class NewtonsoftJsonResult : ContentResult
 {
     public static JsonSerializerSettings DefaultSerializerSettings = JsonNetHelpers.DefaultSerializerSettings;
