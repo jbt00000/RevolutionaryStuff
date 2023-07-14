92b36bd8add71df40fd3e317f56cc901a013698d
Jason Thomas <jason@traffk.com>
2023-07-14 12:12:23 -0400

    Leaving a queue or disconnecting

--- src/RevolutionaryStuff.Data.JsonStore/Entities/IJsonEntityIdServices.cs
+++ src/RevolutionaryStuff.Data.JsonStore/Entities/IJsonEntityIdServices.cs
@@ -4,4 +4,29 @@ public interface IJsonEntityIdServices
 {
     string CreateId(Type entityDataType, string? name = null);
     void ThrowIfInvalid(Type entityDataType, string id);
+
+    #region Default Implementation
+
+    void ThrowIfNotNullAndInvalid(Type entityDataType, string id)
+    {
+        if (id != null)
+        {
+            ThrowIfInvalid(entityDataType, id);
+        }
+    }
+
+    void ThrowIfInvalid<T>(string id)
+        where T : JsonEntity
+        => ThrowIfInvalid(typeof(T), id);
+
+    void ThrowIfNotNullAndInvalid<T>(string id)
+        where T : JsonEntity
+    {
+        if (id != null)
+        {
+            ThrowIfInvalid(typeof(T), id);
+        }
+    }
+
+    #endregion
 }
