e11659af4db502074b2f0e022ab431b388214b7d
Jason Thomas <jason@jasonthomas.com>
2025-05-19 21:34:08 -0400

    IHttpMessageSender helpers

--- src/RevolutionaryStuff.Core/Services/Http/IHttpMessageSender.cs
+++ src/RevolutionaryStuff.Core/Services/Http/IHttpMessageSender.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net.Http;
 using System.Threading;
 
 namespace RevolutionaryStuff.Core.Services.Http;
@@ -8,5 +9,41 @@ public interface IHttpMessageSender
     const string DefaultCorrelationIdHeaderKey = "x-correlation-id";
 
     Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default);
-}
 
+    #region Helpers
+
+    Task<HttpResponseMessage> GetAsync(Uri u)
+        => SendAsync(new(HttpMethod.Get, u));
+
+    async Task<string> GetStringAsync(Uri u, bool throwOnNonSuccess = true)
+    {
+        var resp = await GetAsync(u);
+        if (throwOnNonSuccess)
+            resp.EnsureSuccessStatusCode();
+        return await resp.Content.ReadAsStringAsync();
+    }
+
+    async Task<Stream> GetStreamAsync(Uri u, bool throwOnNonSuccess = true)
+    {
+        var resp = await GetAsync(u);
+        if (throwOnNonSuccess)
+            resp.EnsureSuccessStatusCode();
+        return await resp.Content.ReadAsStreamAsync();
+    }
+
+    async Task<GetDataResult> GetDataAsync(Uri u, bool throwOnNonSuccess = true)
+    {
+        var resp = await GetAsync(u);
+        if (throwOnNonSuccess)
+            resp.EnsureSuccessStatusCode();
+        return new(await resp.Content.ReadAsStreamAsync(), resp);
+    }
+
+    public record GetDataResult(Stream Stream, HttpResponseMessage Message)
+    {
+        public string ContentType => Message.Content.Headers.ContentType?.ToString() ?? MimeType.Application.OctetStream.PrimaryContentType;
+        public long? ContentLength => Message.Content.Headers.ContentLength;
+    }
+
+    #endregion
+}
