d43c8e8155d3afdd725786350c961a5ed7d92080
Jason Thomas <jason@jasonthomas.com>
2025-08-05 16:52:19 -0400

    nuget

---
+++ src/RevolutionaryStuff.Crm.OpenPhone/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+using RevolutionaryStuff.Core.ApplicationParts;
+
+[assembly: InternalsVisibleTo("RevolutionaryStuff.Crm.OpenPhone.Tests")]
+[assembly: AssemblySettingsResourceAutoDiscovery]
