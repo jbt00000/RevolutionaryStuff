160908b4d1379925f9b1c62c2d96d28f344555c3
Jason Thomas <jason@jasonthomas.com>
2026-07-18 20:45:56 -0400

    nuget rsllc 4.170.100

---
+++ src/RevolutionaryStuff.Azure/HierarchicalKeyVaultSecretManager.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace RevolutionaryStuff.Azure;
+
+public class HierarchicalKeyVaultSecretManager : KeyVaultSecretManager
+{
+    private const string PrefixSeparator = "-";
+    private const string SegmentSeparator = "--";
+
+    private readonly string AppNamePrefix;
+    private readonly Func<string, string> KeyNameTransformer;
+
+    public HierarchicalKeyVaultSecretManager(string appName = null, Func<string, string> keyNameTransformer = null)
+    {
+        appName = appName.TrimOrNull();
+        if (appName == null)
+        {
+            AppNamePrefix = "";
+        }
+        else
+        {
+            AppNamePrefix = appName + PrefixSeparator;
+        }
+        KeyNameTransformer = keyNameTransformer;
+    }
+
+    public override string GetKey(KeyVaultSecret secret)
+    {
+        var name = secret.Name[AppNamePrefix.Length..].Replace(SegmentSeparator, ConfigurationPath.KeyDelimiter);
+        if (name.ToLower().Contains("apikey"))
+        {
+            Stuff.NoOp(secret);
+        }
+        name = KeyNameTransformer?.Invoke(name) ?? name;
+        return name;
+    }
+
+    public override bool Load(SecretProperties secret)
+        => secret.Enabled == true && (string.IsNullOrEmpty(AppNamePrefix) || secret.Name.StartsWith(AppNamePrefix));
+}
+
+
